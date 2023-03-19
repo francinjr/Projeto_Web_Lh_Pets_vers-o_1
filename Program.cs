ac7ab05d8efef3d3faddde5facf48ba8e6d32570
francinjr <francinaldomanoel135@gmail.com>
2023-03-19 12:46:55 -0300

    LH Pets

---
+++ Program.cs
@@ -0,0 +1,25 @@
+namespace Projeto_Web_Lh_Pets_versão_1;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        var app = builder.Build();
+
+        app.MapGet("/", () => "Projeto Web - LH Pets versão 1");
+
+        app.UseStaticFiles();
+
+        app.MapGet("/index", (HttpContext contexto) => {
+            contexto.Response.Redirect("index.html", false);
+        });
+
+        Banco dba = new Banco();
+        app.MapGet("/listaClientes", (HttpContext contexto) => {
+            contexto.Response.WriteAsync(dba.GetListaString());
+        });
+
+        app.Run();
+    }
+}
